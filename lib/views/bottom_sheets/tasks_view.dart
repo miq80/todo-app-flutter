cde2e2741bb5d4e9489b1ea2c146e3e3fe94d698
miq80 <mikoladobriy@gmail.com>
2023-11-20 02:37:52 +0200

    initial commit

--- lib/views/bottom_sheets/tasks_view.dart
+++ lib/views/bottom_sheets/tasks_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo_application/components/my_button.dart';
-import 'package:todo_application/components/my_textfield.dart';
 import 'package:todo_application/todo/todo_bloc.dart';
+import 'package:todo_application/todo/todo_model.dart';
 
 class TasksView extends StatelessWidget {
   TasksView({super.key});
@@ -10,8 +9,54 @@ class TasksView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      ,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo App'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _textController,
+              decoration: const InputDecoration(
+                labelText: 'New Todo',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              todoBloc.addTodo(_textController.text);
+              _textController.clear();
+            },
+            child: const Text('Add Todo'),
+          ),
+          StreamBuilder<List<Todo>>(
+            stream: todoBloc.todos,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Text('No todos yet.');
+              }
+              return Column(
+                children: snapshot.data!.map((todo) {
+                  return ListTile(
+                    title: Text(todo.title),
+                    trailing: Checkbox(
+                      value: todo.isCompleted,
+                      onChanged: (isChecked) {
+                        todoBloc.toggleTodo(todo.id);
+                      },
+                    ),
+                    onTap: () {
+                      todoBloc.removeTodo(todo.id);
+                    },
+                  );
+                }).toList(),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
