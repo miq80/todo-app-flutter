a192381447f01cbc645d71123ae42afc364a2f50
miq80 <mikoladobriy@gmail.com>
2023-11-29 11:57:50 +0200

    initial commit

--- lib/todo/todo_model.dart
+++ lib/todo/todo_model.dart
@@ -2,18 +2,27 @@ class Todo {
   final String id;
   final String title;
   final bool isCompleted;
+  final String? description;
+  final DateTime time;
 
-  Todo({required this.id, required this.title, this.isCompleted = false});
+  Todo(
+      {required this.id,
+      required this.title,
+      this.isCompleted = false,
+      this.description,
+      required this.time});
 
-  Todo copyWith({
-    String? id,
-    String? title,
-    bool? isCompleted,
-  }) {
+  Todo copyWith(
+      {String? id,
+      String? title,
+      bool? isCompleted,
+      String? description,
+      DateTime? time}) {
     return Todo(
-      id: id ?? this.id,
-      title: title ?? this.title,
-      isCompleted: isCompleted ?? this.isCompleted,
-    );
+        id: id ?? this.id,
+        title: title ?? this.title,
+        isCompleted: isCompleted ?? this.isCompleted,
+        description: description ?? this.description,
+        time: time ?? this.time);
   }
 }
